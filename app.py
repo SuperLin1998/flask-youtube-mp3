68da6c79189d7c23d2a07f3b49635978364acd83
SuperLin1998 <steven23435@gmail.com>
2025-03-06 15:07:10 +0800

    Fixed bug and updated code

--- app.py
+++ app.py
@@ -49,6 +49,6 @@ if __name__ == '__main__':
     app.run(debug=True)
 """
 
-if name == '__main__':
+if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
